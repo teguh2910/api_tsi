6e08664d00767519cea40f50b8d6e656208f805b
khaironbiz <khaironbiz@gmail.com>
2023-01-29 09:11:44 +0700

    konsultasi

--- app/Models/Consultation.php
+++ app/Models/Consultation.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
 
 class Consultation extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens,SoftDeletes;
+    protected $connection   = 'mongodb';
+    protected $primaryKey   = '_id';
+    protected $dates        = ['deleted_at'];
+    protected $guarded      = [];
 }
