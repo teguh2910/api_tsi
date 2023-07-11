91bff149cb5954409ca15d22fa787064136cf002
khaironbiz <khaironbiz@gmail.com>
2023-07-11 19:14:55 +0700

    update foto profile

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -25,36 +25,41 @@ class FileController extends Controller
             $data = [
                 'errors' => $validator->errors(),
             ];
-        }
-        $file = $request->file('file');
-        $result     = Storage::disk('public')->putFileAs('image', $file, $file->hashName());
-        $url        = Storage::disk('public')->url($result);
-        $data_file  = [
-            'name'      => $file->hashName(),
-            'extention' => $file->getClientOriginalExtension(),
-            'mimeType'  => $file->getClientMimeType(),
-            'size'      => $file->getSize(),
-            'user_id'   => Auth::id(),
-            'url'       => url($url)
+        }else{
+            $file = $request->file('file');
+            $result     = Storage::disk('public')->putFileAs('image', $file, $file->hashName());
+            $url        = Storage::disk('public')->url($result);
+            $data_file  = [
+                'name'      => $file->hashName(),
+                'extention' => $file->getClientOriginalExtension(),
+                'mimeType'  => $file->getClientMimeType(),
+                'size'      => $file->getSize(),
+                'user_id'   => Auth::id(),
+                'url'       => url($url)
+            ];
+            if(!empty($url)){
+                $create_file = new File();
+                $save = $create_file->create($data_file);
+                if($save){
+                    $user = Auth::user();
+                    $update = $user->update([
+                        'foto'  => url($url)
+                    ]);
+                    $status_code    = 200;
+                    $message        = "Foto Profile updated";
+                    $data = [
+                        'foto' => url($url),
+                    ];
+                }
 
-        ];
-        if(!empty($url)){
-            $create_file = new File();
-            $save = $create_file->create($data_file);
-            if($save){
-                $user = Auth::user();
-                $update = $user->update([
-                    'foto'  => url($url)
-                ]);
             }
-            return response()->json([
-                'status_code'   => 200,
-                'message'       => 'success',
-                'data'          => [
-                    'file'      => url($url)
-                ]
-            ]);
         }
+        $response = [
+            'status_code'   => $status_code,
+            'message'       => $message,
+            'data'          => $data
+        ];
+        return response()->json($response);
     }
     public function show(Request $request)
     {
